a48f16a7e8f0c4408af4ea1e03dca6875067cd5f
Emiliano Gaitan <a13491074@itmexicali.edu.mx>
2018-03-21 13:28:47 -0700

    guardar datos al rotar dispositivo

--- app/src/main/java/com/demo/simplecalculator/MainActivity.java
+++ app/src/main/java/com/demo/simplecalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.demo.simplecalculator;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,10 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     EditText input_a, input_b;
     Button btn_calculate_add;
     TextView text_result;
+    String resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         input_b = (EditText) findViewById(R.id.input_text_b);
         btn_calculate_add = (Button) findViewById(R.id.btn_calculate_add);
         text_result = (TextView) findViewById(R.id.text_result);
+
 
         btn_calculate_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,4 +40,23 @@ public class MainActivity extends AppCompatActivity {
     private int calculateAdd(int a, int b) {
         return a + b;
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle guardaEstado) {
+        super.onSaveInstanceState(guardaEstado);
+        //guardamos en la variable t el texto del campo EditText
+        resultado = text_result.getText().toString();
+        //lo "guardamos" en el Bundle
+        guardaEstado.putString("text", resultado);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle recuperaEstado) {
+        super.onRestoreInstanceState(recuperaEstado);
+        //recuperamos el String del Bundle
+        resultado = recuperaEstado.getString("text");
+        //Seteamos el valor del EditText con el valor de nuestra cadena
+        text_result.setText(resultado);
+    }
+
 }
